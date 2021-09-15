98a066859e726e39027690663410a496e7c7f8c8
mariyasada <mariyasada2406@gmail.com>
2021-09-15 15:06:33 +0530

    doc: adding hypontenuse,area, quiz of triangle

---
+++ area.js
@@ -0,0 +1,24 @@
+const sides = document.querySelectorAll(".sides");
+const calculateArea = document.querySelector("#Area-button");
+const outputEl= document.querySelector("#output");
+
+// function calculateproductOfBaseHeight(a,b){
+//     const productofBaseandHeight = a * b;
+//     return productofBaseandHeight;
+// }
+
+
+// calculateArea.addEventListener("click",()=>{
+//     // console.log("clicked");
+//     const productofBaseandHeight = calculateproductOfBaseHeight (Number(sides[0].value),Number(sides[1].value));
+//     const area = (0.5 *(productofBaseandHeight));
+//     outputEl.innerText = "The Area of Triangle is   "  + area + " cm²";
+// });
+
+
+calculateArea.addEventListener("click",()=>{
+    // console.log("clicked");
+    const area = 0.5 * (Number(sides[0].value) * Number(sides[1].value));
+    // const area = (0.5 *(productofBaseandHeight));
+    outputEl.innerText = "The Area of Triangle is   "  + area + " cm²";
+});
---
+++ hypotenuse.js
@@ -0,0 +1,25 @@
+const sides = document.querySelectorAll(".sides");
+const calculateHypo = document.querySelector("#Hypotenuse-button");
+const outputEl= document.querySelector("#output");
+
+function calculateSquare( a, b){
+  const sumOfSquare = (a * a )  +  ( b * b);
+  return sumOfSquare;
+}
+
+
+calculateHypo.addEventListener("click",() => {
+
+// console.log("clicked");
+
+  if((sides[0].value && sides[1].value) ===""){
+    outputEl.innerText = "Please Enter the value in both fields";
+
+  }
+    const sumOfSquare = calculateSquare(Number(sides[0].value),Number(sides[1].value));
+    // console.log(sumOfSquare);
+    const Hypotenuse = Math.sqrt(sumOfSquare);
+
+    outputEl.innerText = "The length of hypotenuse is  " + Hypotenuse;
+
+});
